025597e3f6873c87455159092f51a086b9ac0a18
kesoglidis <gkesoglidis@outlook.com>
2024-12-09 15:29:43 +0200

    Using CAISO Fuel Mix API

--- src/data_model.py
+++ src/data_model.py
@@ -1,6 +1,50 @@
 import json
+import requests
+import time
+from datetime import datetime, timezone
 
 
+# Function to get live stock data for a symbol
+def get_data():
+    response = requests.get(f"https://api.gridstatus.io/v1/datasets/caiso_fuel_mix?api_key=2bf76a052c0649929fb96aad00a08fb3&limit=1000")
+    time.sleep(1)
+    end = str(response.json()['latest_available_time'])
+    # print(end)
+
+    # url = f"https://api.gridstatus.io/v1/datasets/caiso_fuel_mix/query?api_key=d561020ab3e44c32a3e605a2a777593d&limit=1000"
+    url = f"https://api.gridstatus.io/v1/datasets/caiso_fuel_mix/query?start_time=2024-12-09T00:00Z&end_time=" + end + "&api_key=d561020ab3e44c32a3e605a2a777593d&limit=1000"
+    # url = f"https://api.gridstatus.io/v1/dataset-updates/caiso_fuel_mix?api_key=2bf76a052c0649929fb96aad00a08fb3&limit=1000" #updates
+    response = requests.get(url)
+    # response = requests.get(f"https://api.gridstatus.io/v1/datasets/caiso_standardized_5_min?api_key=2bf76a052c0649929fb96aad00a08fb3&limit=1000")
+    print(response)
+
+    # Check if the response is successful
+    if response.status_code == 200:
+        data = response.json()
+        # print(data['data'][0]['id'])
+        # for d in data['data']:
+        #     print(d['id'])
+        # print(" ")
+        # print(data.keys())
+        # for i in data:
+        #     print(i)
+        #     print(type(data[i]))
+        #     if(type(data[i]) == list):
+        #         print(data[i][0].keys())
+        #     else:
+        #         if(type(data[i]) == dict):
+        #             print(data[i].keys())
+        #         else:
+        #             print(data[i])
+        #     print(" ")
+        
+        return data['data']
+    else:
+        return None
+
+
+# data = get_data()
+# print(data)
 class Dataset:
 
     def __init__(self, time, temperature):
@@ -17,3 +61,5 @@ class Dataset:
     def __repr__(self):
         res = self.__dict__()
         return json.dumps(res, indent=2, default=str)
+    
+    
--- src/producer.py
+++ src/producer.py
@@ -1,12 +1,14 @@
 import os
 import random
 import time
-from datetime import datetime
+import json
+from datetime import datetime, timezone
 
 # import confluent_kafka
 from confluent_kafka import Producer
 
 from data_model import Dataset
+from data_model import get_data
 
 if __name__ == "__main__":
     # Get kafka connection string
@@ -16,16 +18,50 @@ if __name__ == "__main__":
     # Conf for kafka connection
     conf = {'bootstrap.servers': KAFKA_BROKER,'client.id': 'myproducer'}
     # Create the producer
+    
     producer = Producer(conf)
+    check = True
+    while check:
+        data = get_data()
+        # print(data)
+        if data != None:
+            old = data[0]['interval_start_utc']
+            check = False
+        time.sleep(5)
+
+    old = "2024-12-09T00:00:00+00:00"
+    print(old)
     while True:
+        start = datetime.now()
+        # print(start)
+        data = get_data()
+        if data != None:
+            # print(data[-1])
+            # print(data[0])
+            i = -1
+            # print(data[0]['interval_start_utc'])
+            # print(data[-1]['interval_start_utc'])
+            # print(old)
+            while data[i]['interval_start_utc'] != old:
+                i -= 1
+                print(i)
+                data[i]['interval_start_utc']
+                msg = data[i]    
+                # print(msg)
+                msg = json.dumps(msg, indent=2, default=str)
+
+                print(f'Publishing: {msg}')
+                producer.produce(KAFKA_TOPIC, msg)
+            old = data[-1]['interval_start_utc']
+        # print(datetime.now()-start)
         # Get a random value
-        temp = random.uniform(15.0, 40.0)
-        # Create the object
-        temperature = Dataset(datetime.now(), temp)
-        # Get the msg in json format
-        msg = temperature.__repr__()
-        # Publish the msg
-        print(f'Publishing: {msg}')
-        producer.produce(KAFKA_TOPIC, msg)
-        # Wait 1 second and publish again
-        time.sleep(1)
+        # temp = random.uniform(15.0, 40.0)
+        # # Create the object
+        # temperature = Dataset(datetime.now(), temp)
+        # # Get the msg in json format
+        # msg = temperature.__repr__()
+        # # Publish the msg
+        # print(f'Publishing: {msg}')
+        # producer.produce(KAFKA_TOPIC, msg)
+        # # Wait 1 second and publish again
+        time.sleep(10)
